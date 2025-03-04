2eec6dc120422f34b7726a4e29e616eca929f2de
Moshe Gelberg <gelberg@pm.me>
2025-03-04 23:33:58 +0200

    Added signin

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -8,17 +8,43 @@ const User = mongoose.model('User');
 const router = express.Router();
 
 router.post('/signup', async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
+	if (!email || !password) {
+		res.status(400).send({ error: 'Email and password are required' });
+		return;
+	}
+
+	try {
+		const user = new User({ email, password });
+		await user.save();
+		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+		res.send({ token });
+	} catch (err: any) {
+		res.status(400).send(err.message);
+	}
+});
+
+router.post('/signin', async (req: Request, res: Response) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		res.status(400).send({ error: 'Email and password are required' });
+		return;
+	}
+
+	const user = await User.findOne({ email });
+	if (!user) {
+		res.status(404).send({ error: 'User not found' });
+		return;
+	}
 
   try {
-    const user = new User({ email, password });
-    await user.save();
-    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-    res.send({ token });
-  } catch (err: any) {
-    res.status(400).send(err.message);
-  }
-
+		await user.comparePassword(password);
+		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+		res.send({ token });
+	} catch (err) {
+		res.status(401).send({ error: 'Invalid email or password' });
+		return;
+	}
 });
 
 export default router;
