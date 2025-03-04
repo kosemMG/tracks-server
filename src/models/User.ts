bc2ee9e8e951d3f2be8fe471ebf36146e30414c8
usf72399 <gelberg@pm.me>
2025-03-04 16:06:12 +0200

    Added bcrypt password hashing

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -12,6 +13,32 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
 
-export default User;
+  bcrypt.genSalt(10, (err: Error | undefined, salt: string) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err: Error | undefined, hash: string) => {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword: string) {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err: Error | undefined, isMatch: boolean) => {
+      if (err) reject(err);
+      if (!isMatch) reject(false);
+
+      resolve(true);
+    });
+  });
+};
+
+mongoose.model('User', userSchema);
