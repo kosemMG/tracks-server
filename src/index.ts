6cf92293710d993369437854c89ee2037dd0177c
Moshe Gelberg <gelberg@pm.me>
2025-03-04 08:14:58 +0200

    Added MongoDB support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.kvcid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+	console.log('MongoDB Connected');
+});
+mongoose.connection.on('error', (err) => {
+	console.error('Error connecting to MongoDB', err);
+})
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello, TypeScript!');
